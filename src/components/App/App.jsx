056d90713ba72624ad4b2455798db17a2de82317
Marina Kiryanova <martinkazz@yandex.ru>
2024-02-12 20:06:06 +0300

    level-3 fix app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -147,6 +147,7 @@ export default function App() {
       .then((user) => {
         setCurrentUser(user);
         setErrorMessages("");
+        setIsSuccessful(true);
       })
       .catch((err) => {
         setErrorMessages(err);
